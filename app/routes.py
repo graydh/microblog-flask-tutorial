908c419073be4ce207f9ee453923b10e289e5dfe
Declan Gray-Mullen <declangraymullen@gmail.com>
2024-03-13 09:37:33 -0400

    Ch 2
    add base page template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
+from flask import render_template
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'Declan'}
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Amherst!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Oppenheimer summer 2023!'
+        }
+    ]
+
+    return render_template('index.html', title="Home page", user=user, posts=posts)
