47818d4379736255e845eb192fe1c5fa3d3e267b
Declan Gray-Mullen <declangraymullen@gmail.com>
2024-03-18 17:37:42 -0400

    ch 7
    error handling: prod html errors, email notification and debug browser panes

--- config.py
+++ config.py
@@ -6,3 +6,10 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-flask-key'
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(base_dir, 'app.db')
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT') or 25
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_MAILUSERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['declangraymullen@gmail.com']
---
+++ logs/microblog.py
@@ -0,0 +1,1 @@
+2024-03-18 10:09:31,822 INFO: Microblog startup [in /Users/dgraymullen/Documents/GitHub/microblog-flask-tutorial/app/__init__.py:44]
