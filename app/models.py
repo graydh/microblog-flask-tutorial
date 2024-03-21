9b72925baf957a9d2cee9c9db525bb17a2a0782e
Declan Gray-Mullen <declangraymullen@gmail.com>
2024-03-21 15:00:05 -0400

    ch 10
    email psswd reset

--- app/models.py
+++ app/models.py
@@ -1,12 +1,15 @@
 from datetime import datetime, timezone
 from hashlib import md5
+import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 from typing import Optional
+from time import time
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from flask_login import UserMixin
 from app import db
 from app import login
+from app import app
 
 followers = sa.Table(
     'followers',
@@ -92,6 +95,18 @@ class User(UserMixin, db.Model):
     @login.user_loader
     def load_user(id):
         return db.session.get(User, int(id))
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in}, app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return db.session.get(User, id)
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
