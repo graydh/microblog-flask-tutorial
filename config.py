2471bd0aa96960812da28102870d09d14365bd03
Declan Gray-Mullen <declangraymullen@gmail.com>
2024-04-05 10:35:19 -0400

    Sendgrid email support

--- config.py
+++ config.py
@@ -16,9 +16,9 @@ class Config:
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = os.environ.get('MAIL_PORT') or 25
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_MAILUSERNAME')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['declangraymullen@gmail.com']
+    ADMINS = ['decgray-mullen@comcast.net']
 
     POSTS_PER_PAGE = 20
 
