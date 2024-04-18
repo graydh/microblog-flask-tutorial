78219dbcdf78a39a5ad1b398f7925822533aa2b9
Declan Gray-Mullen <declangraymullen@gmail.com>
2024-04-18 12:02:53 -0400

    repo cleanup
    remove .idea and logs dir

--- logs/microblog.py
+++
@@ -1,4 +0,0 @@
-2024-04-03 10:50:11,719 INFO: Microblog startup [in /Users/dgraymullen/Documents/GitHub/microblog-flask-tutorial/app/__init__.py:100]
-2024-04-03 10:50:15,617 INFO: Microblog startup [in /Users/dgraymullen/Documents/GitHub/microblog-flask-tutorial/app/__init__.py:100]
-2024-04-03 11:26:58,605 INFO: Microblog startup [in /Users/dgraymullen/Documents/GitHub/microblog-flask-tutorial/app/__init__.py:100]
-2024-04-03 11:53:19,486 INFO: Microblog startup [in /Users/dgraymullen/Documents/GitHub/microblog-flask-tutorial/app/__init__.py:100]
