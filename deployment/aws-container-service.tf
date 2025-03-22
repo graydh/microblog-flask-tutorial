3635b2ec989402e393ff7b1110ba7bd50d4209ef
Declan Gray-Mullen <declangraymullen@gmail.com>
2025-03-22 16:45:45 -0400

    ops: fix tofu var for aws

--- deployment/aws-container-service.tf
+++ deployment/aws-container-service.tf
@@ -14,7 +14,7 @@ variable "AWS_ROLE_ARN" {
 
 provider aws {
   assume_role {
-    role_arn     = var.role_arn
+    role_arn     = var.AWS_ROLE_ARN
     session_name = "UpdateContainerServiceInstance"
     external_id  = "OpenTofu-Dev"
   }
